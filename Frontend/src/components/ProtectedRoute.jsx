8b16254dcbd3e48a6dbedf0ada6fa0c670069792
jack9801 <shishupaljnvr@gmail.com>
2025-08-08 22:26:11 +0530

    protected

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -2,10 +2,15 @@ import { useAuth } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children, adminOnly = false }) {
-  const { isAuthenticated, user } = useAuth();
+  const { isAuthenticated, role } = useAuth(); 
 
-  if (!isAuthenticated) return <Navigate to="/login" />;
-  if (adminOnly && user.role !== 'Admin') return <Navigate to="/unauthorized" />;
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+  
+  if (adminOnly && role !== 'Admin') {
+    return <Navigate to="/candidates" />; 
+  }
 
   return children;
 }
